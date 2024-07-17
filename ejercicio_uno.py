1b21fb2eaf2c58509e7c3d620d6924f2491765d3
daraeulalia31 <DaraEulalia31@gmail.com>
2024-07-17 10:13:22 -0500

    solucion:ejercicio uno

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,22 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+def encontrar_minimo_maximo(lista):
+    if not lista:
+        return None
+
+    menor = lista[0]
+    mayor = lista[0]
+
+    for num in lista:
+        if num < menor:
+            menor = num
+        if num > mayor:
+            mayor = num
+
+    return {"menor": menor, "mayor": mayor}
+
+# Ejemplo de uso
+numeros = [4, 7, 10, 4, 1, 0]
+resultado = encontrar_minimo_maximo(numeros)
+print(resultado)
